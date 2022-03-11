f04f34a01ed41e8670ca645cb6159ffdca9a4154
Arbab <arbabanjum0347@gmail.com>
2022-03-11 16:56:46 +0500

    Core Datatypes

---
+++ 1 Core Types/index.js
@@ -0,0 +1,4 @@
+var num = 10;
+var word = "10";
+var isTrue = true;
+console.log("Number: " + num + "\tWord: " + word + "\tBoolean: " + isTrue);
---
+++ 1 Core Types/index.ts
@@ -0,0 +1,6 @@
+let num:number = 10;
+let word:string = "10";
+let isTrue:boolean = true;
+
+
+console.log("Number: " + num +"\tWord: " + word +"\tBoolean: " + isTrue);
