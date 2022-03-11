692408f6ad9399ee1dcc2c00017724c18f399c4f
Arbab <arbabanjum0347@gmail.com>
2022-03-11 17:17:54 +0500

    Object Type

---
+++ 2 object Type/index.js
@@ -0,0 +1,10 @@
+var admin = {
+    name: "arbab",
+    age: 22
+};
+var customer = {
+    name: "peter",
+    age: 20
+};
+console.log(admin.name);
+console.log(customer.name);
---
+++ 2 object Type/index.ts
@@ -0,0 +1,15 @@
+type objType = {name:string,age:number};
+
+const admin:{name:string,age:number}={
+    name:"arbab",
+    age:22
+}
+
+const customer:objType={
+    name:"peter",
+    age:20
+}
+
+console.log(admin.name);
+console.log(customer.name);
+
