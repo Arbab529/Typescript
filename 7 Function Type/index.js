302709af4f21f3c6876c76c9ca654c256605ccb4
Arbab <arbabanjum0347@gmail.com>
2022-03-13 12:06:38 +0500

    Function Type

---
+++ 7 Function Type/index.js
@@ -0,0 +1,10 @@
+// function name(params:type):returntype {}
+function abc(a, b) {
+    return a + b;
+}
+console.log(abc(3, 2));
+var a;
+function bcd() {
+    return a;
+}
+console.log(bcd());
---
+++ 7 Function Type/index.ts
@@ -0,0 +1,14 @@
+// function name(params:type):returntype {}
+
+function abc(a:number,b:number):number {
+    return a+b;
+}
+
+console.log(abc(3,2));
+
+var a;
+function bcd():undefined {
+    return a;
+}
+
+console.log(bcd());
